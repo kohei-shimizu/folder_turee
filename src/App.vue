2c3b29ea6e320408b76c268dbadd5285581d2c60
清水康平 <69580414+kohei-shimizu@users.noreply.github.com>
2021-05-12 16:06:57 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <div v-on:contextmenu.prevent="$refs.menu.open">
+      <v-treeview :items="items" item-key="name" open-on-click></v-treeview>
+    </div>
+    <context-menu ref="menu">
+      <li v-on:click="trash_empty()">text1</li>
+      <li>text2</li>
+      <li>text3</li>
+      <li>text4</li>
+      <li>text5</li>
+    </context-menu>
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
+import contextMenu from "vue-context-menu";
+Vue.use(Vuetify);
+Vue.use(contextMenu);
+export default {
+  data: () => ({
+    items: [
+      {
+        id: 1,
+        name: "A",
+        children: [
+          {
+            id: 2,
+            name: "フルーツ",
+            children: [
+              { id: 3, name: "リンゴ" },
+              { id: 4, name: "バナナ" },
+              { id: 5, name: "オレンジ" },
+            ],
+          },
+          {
+            id: 6,
+            name: "野菜",
+            children: [
+              { id: 7, name: "ジャガイモ" },
+              { id: 8, name: "ニンジン" },
+              { id: 9, name: "玉ねぎ" },
+            ],
+          },
+        ],
+      },
+    ],
+  }),
+};
+</script>
+
+<style>
+</style>
